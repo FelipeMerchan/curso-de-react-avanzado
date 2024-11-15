89178ab6fdfef2de20d9030a029854ec9f42c54d
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-11-15 17:32:09 -0500

    2. Cómo implementar Render Props y Composition en React

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,30 @@
-export const ParentComponent = () => {
-  return (
-    <>
-    </>
-  );
+/* Render Props permite a un componente recibir una función
+  como prop y así determinar cómo renderizar su contenido desde
+  otro componente. */
+
+/*
+    configuramos dos componentes, un padre y un hijo. El padre utiliza
+    Render Props para indicar cómo debe el hijo mostrar el contenido.
+    La función render del padre recibe un parámetro (name) y retorna
+    un elemento JSX que muestra un saludo personalizado.
+
+    Este patrón otorga control al componente padre, que decide cómo
+    se presenta el contenido en el hijo, promoviendo la reutilización
+    de código y la separación de lógica.
+  */
+
+interface ChildComponentProps {
+  render: (name: string) => JSX.Element;
+}
+
+export const ChildComponent = ({
+  render,
+}: ChildComponentProps): JSX.Element => {
+  const name = "Felipe";
+
+  return <div>{render(name)}</div>;
+};
+
+export const ParentComponent = (): JSX.Element => {
+  return <ChildComponent render={(name) => <p>Hello, {name}!</p>} />;
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,13 @@
-const ParentComponent = () => {
+import { useState } from "react";
+
+interface ParentComponentProps {
+  render: (data: string[]) => React.ReactNode;
+}
+
+const ParentComponent = ({ render }: ParentComponentProps): JSX.Element => {
+  const [data] = useState<string[]>(["Apple", "Banana"]);
+
+  return <ul>{render(data)}</ul>;
 };
 
 export default ParentComponent;

0a29ea28a2f6ad5a877076e03f30f776d6488073
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-11-16 20:28:33 -0500

    6. Cómo utilizar Custom Hooks en React

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,33 @@
-const UserComponent = () => {
+import { useState } from "react";
+
+function useLoading(initialLoading: boolean = false) {
+  const [isLoading, setIsLoading] = useState(initialLoading);
+
+  return {
+    isLoading,
+    setIsLoading,
+  };
+}
+
+interface UserComponentProps {
+  name: string;
+}
+
+const UserComponent = ({ name }: UserComponentProps): JSX.Element => {
+  const { isLoading, setIsLoading } = useLoading(false);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
+      <p>Hello, {name}!</p>
+      <button onClick={() => setIsLoading(true)}>Simulate loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name="Felipe" />;
 };

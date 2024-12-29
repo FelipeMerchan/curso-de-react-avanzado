84365fd670ce17ba5b90fb93a8bb936961310efe
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-12-29 18:01:48 -0500

    15. useReducer: Store

---
+++ my-use-reducer-app/src/components/TodoList.tsx
@@ -0,0 +1,20 @@
+type Todo = {
+  id: number;
+  text: string;
+};
+
+type State = {
+  todos: Todo[];
+};
+
+type Action =
+  | { type: "ADD_TODO"; payload: string }
+  | { type: "REMOVE_TODO"; payload: number };
+
+const initialState: State = {
+  todos: [],
+};
+
+export const TodoList = () => {
+  return <div>TodoList</div>;
+};
